876ed7068ba63108d7476cde702cf7b901a8ac00
Flavio Lucas Fernandes <flaviolukfer@gmail.com>
2022-09-23 20:01:03 -0300

    first commit

---
+++ src/test/java/br/edu/ifpr/vault/VaultApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifpr.vault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
