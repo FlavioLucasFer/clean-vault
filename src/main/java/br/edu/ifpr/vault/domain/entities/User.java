cf1eb4d7f22d755b26eadcd1f0ecb6eec11b1462
Henrique Gonçalves de Oliveira <48062168+HenriqueGon@users.noreply.github.com>
2022-09-23 20:56:43 -0300

    feat: User entity

---
+++ src/main/java/br/edu/ifpr/vault/domain/entities/User.java
@@ -0,0 +1,54 @@
+package br.edu.ifpr.vault.domain.entities;
+
+import lombok.experimental.FieldDefaults;
+
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+
+@FieldDefaults(level=AccessLevel.PRIVATE)
+public class User {
+
+    @Getter
+    String email;
+
+    @Getter
+    String password;
+
+    public User() {}
+
+    public User(String email, String password) throws Exception {
+        this.setEmail(email);
+        this.setPassword(password);
+    }
+
+    public void setEmail(String email) throws Exception {
+        if (!this.validateEmail(email)) {
+            throw new Exception("Invalid Email");
+        }
+        
+        this.email = email;
+    }
+
+    public void setPassword(String password) throws Exception {
+        if (password.length() < 8 || password.length() > 20) {
+            throw new Exception("Password length must be between 8 and 20 characters");
+        }
+
+        this.password = password;
+    }
+
+    private boolean validateEmail(String email) {
+        boolean result = true;
+        try {
+            InternetAddress emailAddr = new InternetAddress(email);
+            emailAddr.validate();
+        } catch (AddressException ex) {
+            result = false;
+        }
+        return result;
+    }
+
+}
